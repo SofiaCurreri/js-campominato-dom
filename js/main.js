c931bd71121bf9939aa5355fd437a03c81883789
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-02 17:30:25 +0100

    files + consegna (2a parte)

---
+++ js/main.js
@@ -0,0 +1,95 @@
+//Consegna (1a parte)
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+//Consegna (2a parte)
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+//Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+
+
+/**************************************************************************
+*                                                                         *
+*                                 ON LOAD                                 *
+*                                                                         *
+***************************************************************************/
+let playButton = document.getElementById("play");
+const playLevel = document.getElementById("mySelect");
+
+
+
+/**************************************************************************
+ *                                                                         *
+ *                              EVENT LISTENER                             *
+ *                                                                         *
+***************************************************************************/
+playButton.addEventListener(
+    "click",
+    function () {
+        const gridEl = document.getElementById("grid");
+        const chooseLevel = playLevel.value;
+        let dimension;
+
+        if (chooseLevel == "l-1") {
+            dimension = 100;
+        } else if (chooseLevel == "l-2") {
+            dimension = 81;
+        } else {
+            dimension = 49;
+        }
+        createGrid(gridEl, dimension, chooseLevel);
+    }
+);
+
+
+
+/**************************************************************************
+*                                                                         *
+*                                 FUNCTIONS                               *
+*                                                                         *
+***************************************************************************/
+function createGrid(gridEl, dim, chooseLevel) {
+    gridEl.innerHTML = "";
+
+    for (let i = 0; i < dim; i++) {
+        //per creare e aggiungere le celle
+        const square = document.createElement("div");
+        square.classList.add("square", "text-white", "fw-bold");
+
+        //per determinare dimensione celle in base al livello scelto
+        if (chooseLevel == "l-1") {
+            square.classList.add("diff-1");
+        } else if (chooseLevel == "l-2") {
+            square.classList.add("diff-2");
+        } else {
+            square.classList.add("diff-3");
+        }
+
+        //per aggiungere alla griglia la cella appena creata
+        gridEl.append(square);
+
+        //per aggiungere i numeri alle celle
+        const squareNum = i + 1;
+        square.innerHTML = squareNum;
+
+        //per far cambiare colore alla cella se cliccata
+        square.addEventListener(
+            "click",
+            function () {
+                this.classList.toggle("active");
+                console.log("Il numero corrispondente alla cella è ", this.innerHTML);
+            }
+        )
+    }
+}
